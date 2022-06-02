a73b4e819c457cfdb37f51b66a3369eadab701a2
G085012020 <208501@st.takushok-u.ac.jp>
2022-06-02 15:30:59 +0900

    第6回

--- tv-program.js
+++ tv-program.js
@@ -1,87 +1,104 @@
-let data = {
-  "list": {
-    "g1": [
-      {
-        "id": "2022030428673",
-        "event_id": "28673",
-        "start_time": "2022-03-04T04:35:00+09:00",
-        "end_time": "2022-03-04T04:40:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "みんなのうた「ごっつぉさま」／「超変身！ミネラルフォーマーズ」",
-        "subtitle": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "content": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "act": "",
-        "genres": [
-          "0409",
-          "0700",
-          "0504"
-        ]
-      },
-      {
-        "id": "2022030427069",
-        "event_id": "27069",
-        "start_time": "2022-03-04T23:05:00+09:00",
-        "end_time": "2022-03-04T23:10:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "パラスポーツ×アニメ「アニ×パラ」▽パラアルペンスキーテーマ曲江口寿史×ＡＣＣ",
-        "subtitle": "パラスポーツの魅力をアニメで伝える番組。高速滑走に挑む精神力が試されるパラアルペンスキーを描く。キャラ原案：江口寿史／曲：Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ",
-        "content": "パラスポーツの魅力をアニメで伝えるプロジェクトの第１３弾。圧倒的なスピードに挑む「パラアルペンスキー」の世界を江口寿史原案の魅力的なキャラクターで描く。平昌パラリンピック金メダリストの村岡桃佳選手への取材から生まれた主人公・桃は、スピードへの恐怖を克服していく。その壁を越えた先にあるものとは…　テーマ曲　♪「Ｏｎ　Ｙｏｕｒ　Ｍａｒｋ」はＡｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂが手掛けた。",
-        "act": "【声】松本まりか，【出演】Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ，【監督】西村一彦，【脚本】加納新太，【原案】江口寿史",
-        "genres": [
-          "0700"
-        ]
-      }
-    ]
+let b = document.querySelector('#kensaku');
+b.addEventListener('click', kensaku);
+
+// 通信を開始する処理
+function kensaku() {
+	// URL を設定
+  let service=document.querySelectorAll('input[name="service"]');
+  let key1;
+  for(let s of service){
+    if(s.checked){
+      key1=s.value;
+    }
   }
-};
+  let genres=document.querySelectorAll('input[name="genres"]');
+  let key2;
+  for(let g of genres){
+    if(g.checked){
+      key2=g.value;
+    }
+  }
+	let url = 'https://www.nishita-lab.org/web-contents/jsons/nhk/'+key1+'-'+key2+'-j.json';
+
+	// 通信開始
+	axios.get(url)
+		.then(showResult)
+		.catch(showError)
+		.then(finish);
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+	// サーバから送られてきたデータを出力
+	let data = resp.data;
+
+	// data が文字列型なら，オブジェクトに変換する
+	if (typeof data === 'string') {
+		data = JSON.parse(data);
+	}
+
+	// data をコンソールに出力
+	console.log(data);
+
+  let service=document.querySelectorAll('input[name="service"]');
+  let key1;
+  for(let s of service){
+    if(s.checked){
+      key1=s.value;
+    }
+  }
+
+
+  let table = document.querySelector('div#placeholder')
+  
+
+
+
+  if(key1=="g1"){
+    for(let i=0;i<data.list.g1.length;i++){
+      let title = document.createElement('th');
+      title.setAttribute('src', data.list.g1[i].title);
+      let start = document.createElement('th');
+      start.setAttribute('src', data.list.g1[i].start_time);
+      let end = document.createElement('th');
+      end.setAttribute('src', data.list.g1[i].end_time);
+    
+
+    // まとめた div 要素
+      let d = document.createElement('table');
+      d.classList.add('table');
+      d.insertAdjacentElement('beforeend', title);
+      d.insertAdjacentElement('beforeend', start);
+      d.insertAdjacentElement('beforeend', end);
+
+      // d をページに追加
+      table.insertAdjacentElement('afterend', d);
+  }
+}
+  else if(key1=="e1"){
+    for(let i=0;i<data.list.e1.length;i++){
+    let t = document.querySelector('span#title');
+    t.textContent=data.list.e1[i].title;
+    let s = document.querySelector('span#start');
+    s.textContent=data.list.e1[i].start_time;
+    let e = document.querySelector('span#end');
+    e.textContent=data.list.e1[i].end_time;
+  }
+}
+}
+// 通信エラーが発生した時の処理
+function showError(err) {
+	console.log(err);
+}	
+
+// 通信の最後にいつも実行する処理
+function finish() {
+	console.log('Ajax 通信が終わりました');
+}
 
 /////////////////// 課題3-2 はここから書き始めよう
-console.log(data.list.g1.length);
-for(d of data.list.g1){
+
+/*for(d of data){
   console.log(d.title+"/"+d.start_time+"/"+d.end_time);
   let t = document.querySelector('span#title');
     t.textContent=d.title;
@@ -89,5 +106,5 @@ for(d of data.list.g1){
     s.textContent=d.start_time;
   let e = document.querySelector('span#end');
     e.textContent=d.end_time;
-}
+}*/
 
